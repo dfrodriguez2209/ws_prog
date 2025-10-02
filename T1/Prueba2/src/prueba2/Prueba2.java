658a6119ade079bdd0acdd5f4aa353f152983888
dfrodriguez2209 <daniel.fernandez@informatica.iesvalledeljerteplasencia.es>
2025-10-02 11:14:11 +0200

    Prueba2
    Otro proyecto de prueba

---
+++ T1/Prueba2/src/prueba2/Prueba2.java
@@ -0,0 +1,17 @@
+
+package prueba2;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Prueba2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
