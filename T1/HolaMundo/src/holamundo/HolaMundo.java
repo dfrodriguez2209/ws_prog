414a8cfe5443bec13dd03ec9e41c6339db9ddfbd
dfrodriguez2209 <daniel.fernandez@informatica.iesvalledeljerteplasencia.es>
2025-10-02 11:06:09 +0200

    Creación Proyecto HolaMundo
    Se sube la primera versión del proyecto

---
+++ T1/HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,19 @@
+
+package holamundo;
+
+/**
+ * Mi primer programa
+ * @author Daniel
+ * @since 2025
+ */
+public class HolaMundo {
+
+    /**
+     * Método main
+     * @param args
+     */
+    public static void main(String[] args) {     
+        System.out.println("Hola Mundo!!!");
+    }
+
+}
