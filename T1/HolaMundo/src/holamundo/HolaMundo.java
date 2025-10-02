a662c65e0bd646fb6f222c21944638371c34d7ea
dfrodriguez2209 <daniel.fernandez@informatica.iesvalledeljerteplasencia.es>
2025-10-02 11:08:45 +0200

    Actualización del mensaje de saludo
    Mensaje nuevo

--- T1/HolaMundo/src/holamundo/HolaMundo.java
+++ T1/HolaMundo/src/holamundo/HolaMundo.java
@@ -13,7 +13,7 @@ public class HolaMundo {
      * @param args
      */
     public static void main(String[] args) {     
-        System.out.println("Hola Mundo!!!");
+        System.out.println("Hola Mundo!!! Versión 2!!!");
     }
 
 }
